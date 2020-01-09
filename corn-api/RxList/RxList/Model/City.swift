5b14c9aeb4d60e93358b59902483a216136b586a
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2020-01-10 05:59:41 +0900

    디렉토리 분류

---
+++ corn-api/RxList/RxList/Model/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  RxList
+//
+//  Created by 이동건 on 2020/01/10.
+//  Copyright © 2020 이동건. All rights reserved.
+//
+
+import Foundation
+
+struct City: Decodable {
+    var id: Int
+    var country: String
+    var city: String
+}
--- corn-api/RxList/RxList/Views/ViewController.swift
+++ corn-api/RxList/RxList/Views/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
 
